823b8c32feedd6ca45ebb3b3212a9adcb8a418b4
Perfect Makanju <damiperfect@gmail.com>
2017-05-02 15:35:02 +0100

    Project setup

---
+++ app/Events/SendLocation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\User;
+
+class SendLocation implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $location;
+
+    public function __construct($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('location');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+use App\Events\SendLocation;
+use App\User;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/map', function (Request $request) {
+    $lat = $request->input('lat');
+    $long = $request->input('long');
+    $location = ["lat"=>$lat, "long"=>$long];
+    event(new SendLocation($location));
+    return response()->json(['status'=>'success', 'data'=>$location]);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
